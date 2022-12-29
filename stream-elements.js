87cb513b7a896cf2c369ec1e1041920c62c3b8b7
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-30 00:55:11 +0100

    Create stream-elements.js
    Added Skeleton showing the Isomorphic ESModule ESScript Pattern from Frank Lemanschik

---
+++ stream-elements.js
@@ -0,0 +1,8 @@
+/**
+ * Import able as ESModule and raw ESScript
+ **/
+
+const streamFromString = () {};
+
+// lastLine.replace('export ','(').replace(';','})'); then eval
+export { streamFromString };
